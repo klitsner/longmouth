3167d91a14531f201f74c1ebbef8658b29cad244
Samson Klitsner <samsonklitsner@MacBook-Pro-2.attlocal.net>
2025-03-20 17:58:58 -0700

    first commit

---
+++ longmouth.js
@@ -0,0 +1,9 @@
+<script>
+    
+onDocumentLoaded = function() {
+    console.log("Document loaded");
+    // Add your code here
+    // For example, you can change the background color of the body
+    console.log("hello world");   
+}
+</script>
