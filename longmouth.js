e4f7d2c8d197c1d5a1f2778c9b6aa3f734516908
Samson Klitsner <samsonklitsner@MacBook-Pro-2.attlocal.net>
2025-03-20 18:05:34 -0700

    second try

--- longmouth.js
+++ longmouth.js
@@ -1,9 +1,9 @@
-<script>
+document.addEventListener("DOMContentLoaded", function () {
     
-onDocumentLoaded = function() {
-    console.log("Document loaded");
-    // Add your code here
-    // For example, you can change the background color of the body
-    console.log("hello world");   
-}
-</script>
+console.log("Document loaded");
+// Add your code here
+// For example, you can change the background color of the body
+console.log("hello world");   
+
+
+});
